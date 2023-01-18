a162e9ec25e2370d7900de7b1d55e1b84daa520d
Shahad Suman <shahad.sumann@gmail.com>
2023-01-18 11:43:32 +0545

    api and request

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template
+import requests
+app = Flask(__name__)
+@app.route('/')
+def index():
+	url = "https://api.kanye.rest"
+	data = requests.get(url)
+	response = data.json()
+	quote = response["quote"]
+	return render_template("index.html",quote=quote)
+if __name__ == "__main__":
+	app.run(debug=True,port = 8080)
